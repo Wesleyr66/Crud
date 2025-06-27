2990758c8d768c08eca3f4b844a66e970fd299cb
Wesley <wesley.rochao@hotmail.com>
2025-06-27 11:45:00 -0300

    first commit

---
+++ Person/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+namespace Person.Models;
+
+public class PersonModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+}
